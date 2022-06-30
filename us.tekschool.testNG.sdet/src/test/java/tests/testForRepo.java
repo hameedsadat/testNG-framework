3954cb4dcf83a2103f7fa5b395461ef0b334aba7
Sayed <sadatsayedibrahim@gmail.com>
2022-06-29 18:19:02 -0700

    mod add

--- us.tekschool.testNG.sdet/src/test/java/tests/testForRepo.java
+++ us.tekschool.testNG.sdet/src/test/java/tests/testForRepo.java
@@ -7,7 +7,7 @@ public class testForRepo {
 // It is coming from remote testNG repo just created
 		
 		
-		// it is not accepted by dev team, let's work on it again . it is the second try.
+		// it is not accepted by dev team, let's work on it again . it is the second try.ddddd
 	}
 
 }
