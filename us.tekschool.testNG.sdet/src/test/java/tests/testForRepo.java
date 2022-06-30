fe36c2a3283d60428897b757657808610af071de
Sayed <sadatsayedibrahim@gmail.com>
2022-06-29 18:18:20 -0700

    file added

--- us.tekschool.testNG.sdet/src/test/java/tests/testForRepo.java
+++ us.tekschool.testNG.sdet/src/test/java/tests/testForRepo.java
@@ -5,6 +5,9 @@ public class testForRepo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 // It is coming from remote testNG repo just created
+		
+		
+		// it is not accepted by dev team, let's work on it again . it is the second try.
 	}
 
 }
